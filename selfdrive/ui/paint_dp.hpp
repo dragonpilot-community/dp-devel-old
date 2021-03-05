ecef427b02358d5d0d845a3a53d3e6846cb45358
Rick Lan <ricklan@gmail.com>
2021-03-05 10:57:38 +0800

    ui/ init

---
+++ selfdrive/ui/paint_dp.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "ui.hpp"
+
+void ui_draw_df_button(UIState *s);
+void ui_draw_ap_button(UIState *s);
+void ui_draw_rec_button(UIState *s);
+void ui_draw_infobar(UIState *s);
+void ui_draw_blindspots(UIState *s, bool hasInfobar);
+void ui_draw_bbui(UIState *s);
--- selfdrive/ui/sidebar.cc
+++ selfdrive/ui/sidebar.cc
@@ -23,6 +23,26 @@ static void draw_settings_button(UIState *s) {
 static void draw_home_button(UIState *s) {
   const float alpha = s->active_app == cereal::UiLayoutState::App::HOME ? 1.0f : 0.65f;
   ui_draw_image(s, home_btn, "button_home", alpha);
+  if (s->scene.dpIsUpdating) {
+    nvgBeginPath(s->vg);
+    nvgCircle(s->vg, home_btn.x + home_btn.w/2, home_btn.y + home_btn.h/2, 90);
+    nvgFillColor(s->vg, nvgRGBA(255, 255, 255, s->scene.dp_alert_rate));
+    nvgFill(s->vg);
+
+    nvgFillColor(s->vg, nvgRGBA(0, 0, 0, s->scene.dp_alert_rate));
+    nvgFontSize(s->vg, s->scene.dpLocale == "zh-TW"? 60 : s->scene.dpLocale == "zh-CN"? 60 : 46);
+    nvgFontFace(s->vg, "sans-bold");
+    nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
+    nvgTextBox(s->vg, home_btn.x, home_btn.y + home_btn.h/2, home_btn.w,
+      s->scene.dpLocale == "zh-TW"? "更新中" : s->scene.dpLocale == "zh-CN"? "更新中" : "UPDATING",
+      NULL);
+
+    s->scene.dp_alert_rate += 5*s->scene.dp_alert_type;
+
+    if (s->scene.dp_alert_rate <= 0 || s->scene.dp_alert_rate >= 255) {
+      s->scene.dp_alert_type *= -1;
+    }
+  }
 }
 
 static void draw_network_strength(UIState *s) {
@@ -36,12 +56,36 @@ static void draw_network_strength(UIState *s) {
   ui_draw_image(s, {58, 196, 176, 27}, util::string_format("network_%d", img_idx).c_str(), 1.0f);
 }
 
-static void draw_battery_icon(UIState *s) {
-  const char *battery_img = s->scene.deviceState.getBatteryStatus() == "Charging" ? "battery_charging" : "battery";
-  const Rect rect = {160, 255, 76, 36};
-  ui_fill_rect(s->vg, {rect.x + 6, rect.y + 5,
-              int((rect.w - 19) * s->scene.deviceState.getBatteryPercent() * 0.01), rect.h - 11}, COLOR_WHITE);
-  ui_draw_image(s, rect, battery_img, 1.0f);
+//static void draw_battery_icon(UIState *s) {
+//  const char *battery_img = s->scene.deviceState.getBatteryStatus() == "Charging" ? "battery_charging" : "battery";
+//  const Rect rect = {160, 255, 76, 36};
+//  ui_fill_rect(s->vg, {rect.x + 6, rect.y + 5,
+//              int((rect.w - 19) * s->scene.deviceState.getBatteryPercent() * 0.01), rect.h - 11}, COLOR_WHITE);
+//  ui_draw_image(s, rect, battery_img, 1.0f);
+//}
+
+static void draw_sidebar_ip_addr(UIState *s) {
+  const int network_ip_w = 176;
+  const int network_ip_x = 54;
+  const int network_ip_y = 255;
+  nvgFillColor(s->vg, COLOR_WHITE);
+  nvgFontSize(s->vg, 34);
+  nvgFontFace(s->vg, "sans-regular");
+  nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
+  nvgTextBox(s->vg, network_ip_x, network_ip_y, network_ip_w, s->scene.dpIpAddr.c_str(), NULL);
+}
+
+static void draw_sidebar_battery_text(UIState *s) {
+  const int battery_txt_w = 96;
+  const int battery_txt_x = 160;
+  const int battery_txt_y = 305;
+  char battery_str[7];
+  snprintf(battery_str, sizeof(battery_str), "%d%%%s", s->scene.deviceState.getBatteryPercent(), s->scene.deviceState.getBatteryStatus() == "Charging" ? "+" : "-");
+  nvgFillColor(s->vg, COLOR_WHITE);
+  nvgFontSize(s->vg, 44);
+  nvgFontFace(s->vg, "sans-regular");
+  nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
+  nvgTextBox(s->vg, battery_txt_x, battery_txt_y, battery_txt_w, battery_str, NULL);
 }
 
 static void draw_network_type(UIState *s) {
@@ -53,7 +97,7 @@ static void draw_network_type(UIState *s) {
       {cereal::DeviceState::NetworkType::CELL4_G, "4G"},
       {cereal::DeviceState::NetworkType::CELL5_G, "5G"}};
   const int network_x = 50;
-  const int network_y = 273;
+  const int network_y = 303;
   const int network_w = 100;
   const char *network_type = network_type_map[s->scene.deviceState.getNetworkType()];
   nvgFillColor(s->vg, COLOR_WHITE);
@@ -90,13 +134,13 @@ static void draw_metric(UIState *s, const char *label_str, const char *value_str
     nvgTextBox(s->vg, rect.x + 50, rect.y + 50, rect.w - 60, value_str, NULL);
 
     nvgFillColor(s->vg, COLOR_WHITE);
-    nvgFontSize(s->vg, 48);
+    nvgFontSize(s->vg, 46);
     nvgFontFace(s->vg, "sans-regular");
     nvgTextAlign(s->vg, NVG_ALIGN_LEFT | NVG_ALIGN_MIDDLE);
     nvgTextBox(s->vg, rect.x + 50, rect.y + 50 + 66, rect.w - 60, label_str, NULL);
   } else {
     nvgFillColor(s->vg, COLOR_WHITE);
-    nvgFontSize(s->vg, 48);
+    nvgFontSize(s->vg, 46);
     nvgFontFace(s->vg, "sans-bold");
     nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
     nvgTextBox(s->vg, rect.x + 35, rect.y + (strchr(message_str, '\n') ? 40 : 50), rect.w - 50, message_str, NULL);
@@ -110,36 +154,38 @@ static void draw_temp_metric(UIState *s) {
       {cereal::DeviceState::ThermalStatus::RED, 2},
       {cereal::DeviceState::ThermalStatus::DANGER, 3}};
   std::string temp_val = std::to_string((int)s->scene.deviceState.getAmbientTempC()) + "°C";
-  draw_metric(s, "TEMP", temp_val.c_str(), temp_severity_map[s->scene.deviceState.getThermalStatus()], 0, NULL);
+  std::string temp_label = s->scene.dpLocale == "zh-TW"? "溫度" : s->scene.dpLocale == "zh-CN"? "温度" : "TEMP";
+  draw_metric(s, temp_label.c_str(), temp_val.c_str(), temp_severity_map[s->scene.deviceState.getThermalStatus()], 0, NULL);
 }
 
 static void draw_panda_metric(UIState *s) {
   const int panda_y_offset = 32 + 148;
 
   int panda_severity = 0;
-  std::string panda_message = "VEHICLE\nONLINE";
+  std::string car = s->scene.dpLocale == "zh-TW"? "車輛" : s->scene.dpLocale == "zh-CN"? "车辆" : "VEHICLE";
+  std::string panda_message = car + (s->scene.dpLocale == "zh-TW"? "\n已連線" : s->scene.dpLocale == "zh-CN"? "\n在线" : "\nONLINE");
   if (s->scene.pandaType == cereal::PandaState::PandaType::UNKNOWN) {
     panda_severity = 2;
-    panda_message = "NO\nPANDA";
+    panda_message = car + (s->scene.dpLocale == "zh-TW"? "\n已離線" : s->scene.dpLocale == "zh-CN"? "\n离线" : "\nOFFLINE");
   }
-#ifdef QCOM2
   else if (s->started) {
     panda_severity = s->scene.gpsOK ? 0 : 1;
     panda_message = util::string_format("SAT CNT\n%d", s->scene.satelliteCount);
   }
-#endif
 
   draw_metric(s, NULL, NULL, panda_severity, panda_y_offset, panda_message.c_str());
 }
 
 static void draw_connectivity(UIState *s) {
-  static std::map<NetStatus, std::pair<const char *, int>> connectivity_map = {
-      {NET_ERROR, {"CONNECT\nERROR", 2}},
-      {NET_CONNECTED, {"CONNECT\nONLINE", 0}},
-      {NET_DISCONNECTED, {"CONNECT\nOFFLINE", 1}},
-  };
-  auto net_params = connectivity_map[s->scene.athenaStatus];
-  draw_metric(s, NULL, NULL, net_params.second, 180 + 158, net_params.first);
+  if (s->scene.dpAthenad) {
+    static std::map<NetStatus, std::pair<const char *, int>> connectivity_map = {
+        {NET_ERROR, {"CONNECT\nERROR", 2}},
+        {NET_CONNECTED, {"CONNECT\nONLINE", 0}},
+        {NET_DISCONNECTED, {"CONNECT\nOFFLINE", 1}},
+    };
+    auto net_params = connectivity_map[s->scene.athenaStatus];
+    draw_metric(s, NULL, NULL, net_params.second, 180 + 158, net_params.first);
+  }
 }
 
 void ui_draw_sidebar(UIState *s) {
@@ -150,7 +196,8 @@ void ui_draw_sidebar(UIState *s) {
   draw_settings_button(s);
   draw_home_button(s);
   draw_network_strength(s);
-  draw_battery_icon(s);
+  draw_sidebar_ip_addr(s);
+  draw_sidebar_battery_text(s);
   draw_network_type(s);
   draw_temp_metric(s);
   draw_panda_metric(s);
